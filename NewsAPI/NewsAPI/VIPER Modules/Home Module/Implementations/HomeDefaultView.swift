29c5abd19957287a230f9ff2fe68def90238ac69
MarkDestroyer <markkir123@gmail.com>
2022-11-25 01:24:20 +0300

    update
    Added animation for TabBar.

--- NewsAPI/NewsAPI/VIPER Modules/Home Module/Implementations/HomeDefaultView.swift
+++ NewsAPI/NewsAPI/VIPER Modules/Home Module/Implementations/HomeDefaultView.swift
@@ -11,11 +11,17 @@ import UIKit
 class HomeDefaultView: UITabBarController {
     
     var presenter: HomePresenter?
+    var animation = AnimationClass()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         UITabBar.appearance().tintColor = .black
     }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        animation.TabBarItemAnimation(item: item)
+    }
+    
 }
 
 extension HomeDefaultView: HomeView {
