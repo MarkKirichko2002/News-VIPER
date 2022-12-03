f5ea955565a66a6596ee85ceb628031f1bbc53c9
MarkDestroyer <markkir123@gmail.com>
2022-12-03 23:14:06 +0300

    update
    Added splash screen.

--- NewsAPI/NewsAPI/NewsAPIPackage/Sources/NewsAPIPackage/Animation/AnimationClass.swift
+++ NewsAPI/NewsAPI/NewsAPIPackage/Sources/NewsAPIPackage/Animation/AnimationClass.swift
@@ -20,6 +20,28 @@ class AnimationClass {
         propertyAnimator.addAnimations({ barItemView.transform = .identity }, delayFactor: CGFloat(timeInterval))
         propertyAnimator.startAnimation()
     }
+    
+    func springLabel(label: UILabel) {
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.stiffness = 300
+        animation.mass = 1
+        animation.duration = 0.5
+        animation.beginTime = CACurrentMediaTime() + 0
+        label.layer.add(animation, forKey: nil)
+    }
+
+    func springImage(image: UIImageView) {
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.stiffness = 300
+        animation.mass = 1
+        animation.duration = 0.5
+        animation.beginTime = CACurrentMediaTime() + 0
+        image.layer.add(animation, forKey: nil)
+    }
 
 }
 
