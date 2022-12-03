7854e264be5c394b6b05c12832a4eb4bd0f1ec6c
MarkDestroyer <markkir123@gmail.com>
2022-12-03 23:16:07 +0300

    update
    Added splash screen.

---
+++ NewsAPI/NewsAPI/NewsAPIPackage/Sources/NewsAPIPackage/Views/ViewControllers/SplashScreenViewController.swift
@@ -0,0 +1,70 @@
+//
+//  File.swift
+//  
+//
+//  Created by Марк Киричко on 03.12.2022.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+@available(iOS 13.0, *)
+public class SplashScreenViewController: UIViewController {
+    
+    var Image = UIImageView()
+    var Text = UILabel()
+    var animation = AnimationClass()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupView()
+        SetupConstraints()
+        SplashScreen()
+    }
+    
+    func setupView() {
+        Image.image = UIImage(systemName: "newspaper")
+        Image.tintColor = .black
+        self.view.addSubview(Image)
+        Text.textAlignment = .center
+        Text.font = UIFont(name: "Avenir-medium", size: 25)
+        self.view.addSubview(Text)
+    }
+    
+    func SetupConstraints() {
+        
+        Image.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(250)
+            make.height.equalTo(200)
+            make.width.equalTo(200)
+        }
+        
+        Text.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(Image.snp.bottom).offset(20)
+            make.height.equalTo(60)
+            make.leading.equalTo(Image)
+            make.trailing.equalTo(Image)
+        }
+    }
+    
+    func SplashScreen() {
+        
+        animation.springImage(image: Image)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.Text.text = "Новости 2022"
+            self.animation.springLabel(label: self.Text)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            let vc = HomeModule().buildDefault()
+            guard let window = self.view.window else {return}
+            window.rootViewController = vc
+        }
+    }
+    
+}
