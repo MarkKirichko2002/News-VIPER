909a453b5237332da3f3aedbfdc2642e2fc26b3e
MarkDestroyer <markkir123@gmail.com>
2022-12-04 14:06:10 +0300

    update

--- NewsAPI/NewsAPI/NewsAPIPackage/Sources/NewsAPIPackage/NewsAPIPackage.swift
+++ NewsAPI/NewsAPI/NewsAPIPackage/Sources/NewsAPIPackage/NewsAPIPackage.swift
@@ -1,4 +1,11 @@
+import UIKit
+
+public func SCImage(named name: String) -> UIImage? {
+  UIImage(named: name, in: Bundle.module, compatibleWith: nil)
+}
+
 public struct NewsAPIPackage {
+    
     public private(set) var text = "Hello, World!"
 
     public init() {
